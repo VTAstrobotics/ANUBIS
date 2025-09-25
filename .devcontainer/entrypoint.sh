7e393b246ef85885849ecbb0460741d800555179
LeScooot <jeronimus.ryan@gmail.com>
2025-09-25 17:03:42 -0400

    adding devcontainter

---
+++ .devcontainer/entrypoint.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# Set up logging entrypoint stuff to build.log file in .devcontainer directory
+rm /workspaces/ANUBIS/.devcontainer/build.log
+sudo touch /workspaces/ANUBIS/.devcontainer/build.log
+sudo chmod 777 /workspaces/ANUBIS/.devcontainer/build.log
+
+{ printf "entrypoint.sh:\n"
+###############################################################################
+#                                                                             #
+# Startup commands (do not put anything above this)                           #
+#                                                                             #
+###############################################################################
+
+# Set up git mergetool cli
+git config merge.tool vimdiff
+git config merge.conflictstyle diff3
+git config mergetool.prompt false
+
+# ROS basic build and source
+cd /workspaces/ANUBIS/
+source build_scripts/build.sh
+
+# Give permissions to input devices, like Xbox controller
+printf "[INFO]: If you get a message like \"chmod: cannot access '/dev/input/js*': No such file or directory\" do not worry. If it ALSO says \"chmod: cannot access '/dev/input/event*': No such file or directory\" AND you have a controller plugged in, there may be an issue with seeing the controller. Note that the new Jetson Orin Nanos require a manual kernel patch to see Xbox controllers.\n"
+sudo chmod +rx /dev/input/event*
+sudo chmod +rx /dev/input/js*
+
+
+###############################################################################
+#                                                                             #
+# Shutdown commands (do not put anything below this)                          #
+#                                                                             #
+###############################################################################
+} &> /workspaces/REAPER/.devcontainer/build.log
+
+exit 0
