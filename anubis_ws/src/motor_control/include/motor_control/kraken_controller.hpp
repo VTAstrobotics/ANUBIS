7d68c386f0ac6aaf587f153faf1feb7991fa6dd6
cameron <ckiw23@gmail.com>
2025-10-12 02:36:54 +0000

    can create a kraken control node from config file. All that is left is health publishing and hardware testing

--- anubis_ws/src/motor_control/include/motor_control/kraken_controller.hpp
+++ anubis_ws/src/motor_control/include/motor_control/kraken_controller.hpp
@@ -1,24 +1,54 @@
 #pragma once
 #include "motor_control/motor_controller_base.hpp"
+#include <chrono>
+#include <ctre/phoenix6/TalonFX.hpp>
 
 #include <rclcpp/rclcpp.hpp>
+using namespace ctre::phoenix6;
 class KrakenController : public motor_control::MotorControllerBase
 {
 public:
-  explicit KrakenController(const rclcpp::NodeOptions & options = rclcpp::NodeOptions())
-  : motor_control::MotorControllerBase(options){
+  explicit KrakenController(const rclcpp::NodeOptions &options = rclcpp::NodeOptions())
+      : motor_control::MotorControllerBase(options)
+  {
 
     RCLCPP_INFO(this->get_logger(), "KrakenController node has been initialized.");
-    control_subscription_ = this->create_subscription<motor_messages::msg::Command>(
-      this->get_parameter("control_topic").as_string(),
-      10,
-      std::bind(&KrakenController::control_callback, this, std::placeholders::_1)
-    );
+    this->declare_parameter<std::string>("can_interface", "can0");
+    this->declare_parameter<int>("can_id", 42069);
+    this->control_subscription = this->create_subscription<motor_messages::msg::Command>(
+        this->get_parameter("control_topic").as_string(),
+        10,
+        std::bind(&KrakenController::control_callback, this, std::placeholders::_1));
+
+      std::string status_topic = this->get_parameter("status_topic").as_string();
+      std::string health_topic = this->get_parameter("health_topic").as_string();
+      status_publisher = this->create_publisher<motor_messages::msg::Feedback>(status_topic, 4);
+      health_publisher = this->create_publisher<motor_messages::msg::Health>(health_topic, 4);
+
+    std::string can_interface = this->get_parameter("can_interface").as_string();
+    this->motor = std::make_unique<hardware::TalonFX>((int)this->get_parameter("can_id").as_int(), can_interface);
+
+    std::chrono::duration<double> status_period(1 / this->get_parameter("status_publish_frequency").as_double()); // Defaults to seconds
+    this->status_timer = this->create_wall_timer(status_period, std::bind(&KrakenController::publish_status, this));
+
+    std::chrono::duration<double> health_period(1 / this->get_parameter("health_publish_frequency").as_double()); // Defaults to seconds
+    this->health_timer = this->create_wall_timer(health_period, std::bind(&KrakenController::publish_health, this));
+    
+
+
   }
 
-
   void control_callback(const motor_messages::msg::Command::SharedPtr msg) override;
+  void publish_status() override;
+  void publish_health() override;
 
-  private:
-    rclcpp::Subscription<motor_messages::msg::Command>::SharedPtr control_subscription_;
+private:
+    std::unique_ptr<hardware::TalonFX> motor; // INIT just so that there are no warnings, this needs to be overridden
+
+
+
+  controls::DutyCycleOut outDuty{0.0};
+  controls::VelocityVoltage outVelocity{0.0_tps}; // in Turns per second
+  controls::PositionVoltage outPosition{0.0_tr};
+  //Current Requires a premium payment lmao 
 };
--- anubis_ws/src/motor_control/src/kraken_controller.cpp
+++ anubis_ws/src/motor_control/src/kraken_controller.cpp
@@ -1,13 +1,51 @@
 #include "motor_control/motor_controller_base.hpp"
 #include <rclcpp/rclcpp.hpp>
 #include "motor_control/kraken_controller.hpp"
+#include <ctre/phoenix6/TalonFX.hpp>
 
 
 
 
-  void KrakenController::control_callback(const motor_messages::msg::Command::SharedPtr msg)
+  void KrakenController::control_callback(const motor_messages::msg::Command::SharedPtr msg) 
   {    
-    RCLCPP_INFO(this->get_logger(), "Controlling Kraken motor with command: %f", (msg->velocity.data));
-    // Implement the logic to control the Kraken motor here
+
+    if(abs(msg->dutycycle.data) >= 0){
+      this->outDuty.Output = msg->dutycycle.data;
+      this->motor->SetControl(this->outDuty);
+    }
+    else if(abs(msg->current.data) >= 0){
+      RCLCPP_ERROR(this->get_logger(), "We have not paid for this feature L");
+    }
+    else if(abs(msg->position.data) >= 0){
+
+      this->outPosition = msg->position.data * 1.0_tr;
+
+    }
+    else{
+      controls::DutyCycleOut stop{0};
+      this->motor->SetControl(stop);
+    }
+
+  }
+
+  void KrakenController::publish_status() 
+  {
     
+    motor_messages::msg::Feedback feedback;
+    RCLCPP_INFO(this->get_logger(), "Publishing Kraken motor status");
+
+    feedback.current.data = this->motor->GetStatorCurrent().GetValueAsDouble();
+    feedback.is_disabled.data = false;
+    feedback.position.data = this->motor->GetPosition().GetValueAsDouble();
+    feedback.velocity.data = this->motor->GetVelocity().GetValueAsDouble();
+
+    // Implement the logic to publish the Kraken motor status here
+    status_publisher->publish(feedback);
+    
+  }
+
+  void KrakenController::publish_health() 
+  {
+    RCLCPP_INFO(this->get_logger(), "Publishing Kraken motor health");
+    // Implement the logic to publish the Kraken motor health here
   }
