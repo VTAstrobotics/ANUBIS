ce08bc1b77c779890b499e44588f8d2792d53315
cameron <ckiw23@gmail.com>
2025-10-15 18:09:35 +0000

    added the old camera_streamer package

--- anubis_ws/src/astro_image_tools/launch/ffmpeg_encoder.launch.py
+++ anubis_ws/src/astro_image_tools/launch/ffmpeg_encoder.launch.py
@@ -2,20 +2,6 @@ from launch import LaunchDescription
 from launch_ros.actions import Node
 
 def generate_launch_description():
-    cam2 = Node(
-        package='image_tools',
-        executable='cam2image',
-        name='cam2image',
-        output='screen',
-        parameters=[{
-            'camera_device': 0,      # index -> /dev/video0
-            'width': 1280,
-            'height': 720,
-            'frequency': 30.0,
-            'show_image': False
-        }],
-        remappings=[('image', '/camera/image_raw')]
-    )
 
     # ffmpeg params: these are the publisher (encoder) options.
     # Parameter names are "<base_topic>.ffmpeg.<param_name>" (see README).
@@ -25,20 +11,20 @@ def generate_launch_description():
 
         # Comma-separated libav (ffmpeg) options in the form key:value
         # e.g. 'preset:veryfast,profile:baseline,crf:23'
-        'image_raw.ffmpeg.encoder_av_options': 'preset:veryfast,profile:baseline,crf:23',
-
+        'image_raw.ffmpeg.encoder_av_options': 'preset:superfast,profile:baseline,crf:28,tune:zerolatency',
+        # 'crf' is the quality setting for libx264: lower -> better quality (range 0-51, default 23)
         # Number of frames between keyframes (larger -> better compression, more latency)
         'image_raw.ffmpeg.gop_size': 2,
 
         # Target bitrate in bits/s (e.g. 4 Mbps)
-        'image_raw.ffmpeg.bit_rate': 400000,
+        'image_raw.ffmpeg.bit_rate': 40000,
 
         # Force pixel format used for encoding (typical values: 'yuv420p', 'yuvj420p', etc.)
         # This corresponds to the av_source_pixel_format in the encoder-decoder API.
         'image_raw.ffmpeg.pixel_format': 'yuv420p',
 
         # Maximum quantizer (encoder quality control). Larger -> worse image but smaller bitrate.
-        'image_raw.ffmpeg.qmax': 60,
+        'image_raw.ffmpeg.qmax': 150,
 
         # For debugging/perf measurement; set True to enable encoder perf measurements
         'image_raw.ffmpeg.encoder_measure_performance': False,
@@ -62,11 +48,11 @@ def generate_launch_description():
         arguments=['raw', 'ffmpeg'],
         # remap the 'in' and 'out' base topic names expected by republish
         remappings=[
-            ('in', '/camera/image_raw'),
-            ('out', '/camera/image_raw/ffmpeg')
+            ('in', '/usbcam_image_camera'),
+            ('out', '/ffmpeg')
         ],
         # set ffmpeg parameters for the outgoing (ffmpeg) transport
         parameters=[ffmpeg_params]
     )
 
-    return LaunchDescription([cam2, republish])
+    return LaunchDescription([republish])
