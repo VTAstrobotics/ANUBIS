ab134f870ef8f06a7c4a6f588197a4bf4e30bebd
camtriggerhappy <60159291+camtriggerhappy@users.noreply.github.com>
2025-10-31 18:11:02 -0400

    Revert "Spark max ros node"

--- launch_scripts/can_startup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-#from https://canable.io/getting-started.html bc we have a knock off canable
-    sudo slcand -ocs8 /dev/CAN-SERIAL-LINK can0
-    sudo ifconfig can0 up
-    sudo ifconfig can0 txqueuelen 1000
-echo "CAN interface started, haul some rocks"
