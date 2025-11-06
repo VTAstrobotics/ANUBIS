2602d114d9afa61cf9a5914e1ef1bcd9f59a99ca
Hayden11211 <haydenranderson@vt.edu>
2025-11-06 18:06:54 -0500

    added joint names

--- anubis_ws/src/Arm/Arm/ArmHardwareNode.py
+++ anubis_ws/src/Arm/Arm/ArmHardwareNode.py
@@ -4,6 +4,7 @@ from std_msgs.msg import Float64MultiArray
 from rclpy.node import Node
 import time
 from std_msgs.msg import String
+from sensor_msgs.msg import JointState
 
 
 class ArmHardware(Node):
@@ -17,6 +18,7 @@ class ArmHardware(Node):
             10)
       self.subscription_  # prevent unused variable warning
       self.arduino = serial.Serial(port='/dev/ttyACM0', baudrate=9600, timeout=.1)
+      self.joint_pos_publisher = self.create_publisher(JointState, '/joint_states', 10)
 
    def listener_callback(self, msg):
       self.get_logger().info('I heard: "%s"' % msg.data)
@@ -28,6 +30,23 @@ class ArmHardware(Node):
       time.sleep(0.05)
       data = self.arduino.readline()
       print(data)
+      js = JointState()
+      js.header.stamp = self.get_clock().now().to_msg()
+      js.name[0] = "joint0"
+      js.name[1] = "joint1"
+      js.name[2] = "joint2"
+      js.name[3] = "joint3"
+      js.name[4] = "joint4"
+      js.name[5] = "joint5"
+      positions_rad = []
+      for p in msg.data:
+         deg = float(p)
+         rad = deg * math.pi / 180.0
+         positions_rad.append(rad)
+
+      js.position = positions_rad
+
+      self.joint_state_pub.publish(js)
 
 
 
