623a7c6573308d41fe2238c4812496bc072b9e84
Flame875 <awalls03@vt.edu>
2025-11-06 13:14:27 -0500

    Adding the can launch script

--- anubis_ws/src/distributor/launch/motor_distributor_launch.py
+++ anubis_ws/src/distributor/launch/motor_distributor_launch.py
@@ -1,5 +1,6 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
+from launch.actions import ExecuteProcess
 from ament_index_python.packages import get_package_share_directory
 import os
 
@@ -9,6 +10,12 @@ def generate_launch_description():
     pkg_share_distributor = get_package_share_directory("distributor")
     config_file_motors = os.path.join(pkg_share_motors, "config", "kraken_params.yaml")
     config_file_distributor = os.path.join(pkg_share_distributor, "config", "distributorconfig.yaml")
+
+    can_script = os.path.join(pkg_share_distributor, "scripts", "can_startup.sh")
+    can_startup = ExecuteProcess(
+        cmd=["bash", can_script],
+        output="screen"
+    )
 
     # List each node name defined in the YAML
     motor_nodes = ["kraken_front_left", "kraken_front_right"]
