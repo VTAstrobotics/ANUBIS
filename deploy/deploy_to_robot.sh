7a522188698dc215b1e634bb15b5012015c7a81b
cameron <ckiw23@gmail.com>
2025-11-14 14:53:12 -0500

    Tested over SSH on my local PC and it seems to work

--- deploy/deploy_to_robot.sh
+++ deploy/deploy_to_robot.sh
@@ -3,4 +3,22 @@
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 ANUBIS_DIR="$( dirname "$SCRIPT_DIR" )"   # parent of deploy
 
-rsync -avz --delete "$ANUBIS_DIR" /
+Dest_IP="$1"
+
+if [ -z "$Dest_IP" ]; then
+  echo "Usage: $0 user@host:/path"
+  echo
+  echo "Description:"
+  echo "  user@host:/path  The SSH destination where the ANUBIS directory"
+  echo "                   will be synced. Example:"
+  echo "                   ./sync.sh rover@192.168.1.50"
+  exit 1
+fi
+
+
+
+Dest="$Dest_IP":/Deployments/ANUBIS
+
+ssh "$Dest_IP" "mkdir -p /Deployments/ANUBIS"
+
+rsync -avz --delete "$ANUBIS_DIR" "$Dest"
