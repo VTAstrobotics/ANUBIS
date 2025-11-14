1e41ab48ac9ba9c3d61fdf787f3624b5fe894e77
cameron <ckiw23@gmail.com>
2025-11-14 15:08:52 -0500

    added documentation

--- deploy/deploy_to_robot.sh
+++ deploy/deploy_to_robot.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+# This script copies your entire ANUBIS directory to a remote robot
+# over SSH using rsync. It assumes you have SSH access to the robot.
+# This means that you do not need to connect the robot to the internet or to a monitor 
+# unless you are adding external dependencies. The syncing should handle any amount of changes you may be making
+# note that the ANUBIS directory on the robot will be replaced with your local copy.
+# and the Anubis Directory on the robot will be located at /Deployments/ANUBIS
+# The normal Anubis directory will be left alone for any potential edge cases.
+
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 ANUBIS_DIR="$( dirname "$SCRIPT_DIR" )"   # parent of deploy
 
