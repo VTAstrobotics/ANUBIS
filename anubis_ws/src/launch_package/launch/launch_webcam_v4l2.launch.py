a5b074a2b2ff72372b13c59c4c20b250c70b2704
Cameron <ckiw23@gmail.com>
2026-01-18 13:27:27 -0500

    cleaned remenants of copy pasting this launch script

--- anubis_ws/src/launch_package/launch/launch_webcam_v4l2.launch.py
+++ anubis_ws/src/launch_package/launch/launch_webcam_v4l2.launch.py
@@ -9,37 +9,7 @@ from launch.actions import ExecuteProcess
 from launch.conditions import IfCondition, UnlessCondition
 
 def generate_launch_description():
-    # drive_share = get_package_share_directory('drive')
-    pkg_share_distributor = get_package_share_directory('distributor')
-    # web_video_share = get_package_share_directory('web_video_server')
-
-    config_file_distributor = os.path.join(pkg_share_distributor, "config", "distributorconfig.yaml")
-
-    nav2_bringup_share = get_package_share_directory('nav2_bringup')
-    # our_nav_shar = get_package_share_directory('navigation')
-    # nav2_params = os.path.join(our_nav_shar, 'config', 'nav2_params.yaml')
-
-    # nav2_launch = os.path.join(nav2_bringup_share, 'launch', 'navigation_launch.py')
-    # parameters={
-    #       'frame_id':'base_link',
-    #       'use_sim_time':True,
-    #       'subscribe_depth':True,
-    #       'use_action_for_goal':True,
-    #       'Reg/Force3DoF':'true',
-    #       'Grid/RayTracing':'true', # Fill empty space
-    #       'Grid/3D':'false', # Use 2D occupancy
-    #       'Grid/RangeMax':'5',
-    #       'Grid/NormalsSegmentation':'false', # Use passthrough filter to detect obstacles
-    #       'Grid/MaxGroundHeight':'0.05', # All points above 5 cm are obstacles
-    #       'Grid/MaxObstacleHeight':'0.4',  # All points over 1 meter are ignored
-    #       'wait_for_transform_duration': 1,
-    #       'Optimizer/GravitySigma':'0', # Disable imu constraints (we are already in 2D)
-    #       'Grid/FrameId':'map'
-    # }
-
     cameras = []
-
-
 
     webcam_v4l2 = Node(
         package="v4l2_camera",
