7bf1b405483cb51e455c7c00a420bbc1ef9746c7
LeScooot <jeronimus.ryan@gmail.com>
2026-02-17 13:51:23 -0500

    building

---
+++ anubis_ws/src/arm/src/arm_hardware_node.cpp
@@ -0,0 +1,28 @@
+#include <memory>
+#include <chrono>
+
+#include "rclcpp/rclcpp.hpp"
+
+
+
+using std::placeholders::_1;
+class Distributor : public rclcpp::Node
+{
+public:
+  ArmHardwareNode()
+      : Node("arm_hardware_node") // name of the node
+  {
+    
+  }
+
+private:
+  
+};
+
+int main(int argc, char *argv[])
+{
+  rclcpp::init(argc, argv);
+  rclcpp::spin(std::make_shared<ArmHardwareNode>());
+  rclcpp::shutdown();
+  return 0; 
+}
