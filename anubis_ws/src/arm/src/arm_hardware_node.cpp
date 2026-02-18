cb0648f9170fb280a84d0b325920a3f8d35c75e1
LeScooot <jeronimus.ryan@gmail.com>
2026-02-18 11:11:33 -0500

    adding pubs/subs

--- anubis_ws/src/arm/src/arm_hardware_node.cpp
+++ anubis_ws/src/arm/src/arm_hardware_node.cpp
@@ -6,6 +6,7 @@
 #include "motor_messages/msg/command.hpp"
 #include "motor_control/kraken_controller.hpp"
 #include "motor_control/motor_controller_base.hpp"
+#include "std_msgs/msg/float64_multi_array.hpp"
 
 #define MAX_MOTORS 5
 
@@ -14,14 +15,14 @@ enum JOINT
   BASE,
   ELBOW,
   WRIST,
-  END_EFFECTOR,
-  TODO
+  END_EFFECTOR_LAT,
+  END_EFFECTOR_LONG
 };
 
-struct joint_motor_publishers{
+struct joint_motor_publishers
+{
   rclcpp::Publisher<motor_messages::msg::Command>::SharedPtr left_publisher;
   rclcpp::Publisher<motor_messages::msg::Command>::SharedPtr right_publisher;
-
 };
 
 using std::placeholders::_1;
@@ -31,13 +32,19 @@ public:
   ArmHardwareNode()
       : Node("arm_hardware_node") // name of the node
   {
+    add_publishers_to_vector();
+    joint_pos_subscriber = this->create_subscription<std_msgs::msg::Float64MultiArray>(
+        "/cmd_vel", 10, std::bind(&ArmHardwareNode::joint_pos_callback, this, _1));
   }
 
 private:
   joint_motor_publishers motor_publishers[MAX_MOTORS]; // 5 DOF, 2 motors each
 
+  rclcpp::Subscription<std_msgs::msg::Float64MultiArray>::SharedPtr joint_pos_subscriber;
+
   void add_publishers_to_vector()
   {
+
     motor_publishers[BASE].left_publisher = this->create_publisher<motor_messages::msg::Command>("/base_left/control", 10);
     motor_publishers[BASE].right_publisher = this->create_publisher<motor_messages::msg::Command>("/base_right/control", 10);
 
@@ -47,8 +54,16 @@ private:
     motor_publishers[WRIST].left_publisher = this->create_publisher<motor_messages::msg::Command>("/wrist_left/control", 10);
     motor_publishers[WRIST].right_publisher = this->create_publisher<motor_messages::msg::Command>("/wrist_right/control", 10);
 
-    motor_publishers[END_EFFECTOR].left_publisher = this->create_publisher<motor_messages::msg::Command>("/end_effector_left/control", 10);
-    motor_publishers[END_EFFECTOR].right_publisher = this->create_publisher<motor_messages::msg::Command>("/end_effector_right/control", 10);
+    motor_publishers[END_EFFECTOR_LAT].left_publisher = this->create_publisher<motor_messages::msg::Command>("/end_effector__lat_left/control", 10);
+    motor_publishers[END_EFFECTOR_LAT].right_publisher = this->create_publisher<motor_messages::msg::Command>("/end_effector_lat_right/control", 10);
+
+    motor_publishers[END_EFFECTOR_LONG].left_publisher = this->create_publisher<motor_messages::msg::Command>("/end_effector__long_left/control", 10);
+    motor_publishers[END_EFFECTOR_LONG].right_publisher = this->create_publisher<motor_messages::msg::Command>("/end_effector_long_right/control", 10);
+  }
+
+  void joint_pos_callback(std_msgs::msg::Float64MultiArray::SharedPtr msg)
+  {
+
 
   }
 };
