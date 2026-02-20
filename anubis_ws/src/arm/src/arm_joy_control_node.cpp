4ec0dce2cd722a7070e1700d92c97b5bbdac76c5
LeScooot <jeronimus.ryan@gmail.com>
2026-02-20 18:08:12 -0500

    working

--- anubis_ws/src/arm/src/arm_joy_control_node.cpp
+++ anubis_ws/src/arm/src/arm_joy_control_node.cpp
@@ -4,28 +4,23 @@
 #include <cmath>
 
 #include "rclcpp/rclcpp.hpp"
-#include "motor_messages/msg/command.hpp"
-#include "motor_messages/msg/feedback.hpp"
-#include "motor_control/kraken_controller.hpp"
-#include "motor_control/motor_controller_base.hpp"
-#include "std_msgs/msg/float64_multi_array.hpp"
+#include "std_msgs/msg/float32_multi_array.hpp"
 #include "sensor_msgs/msg/joy.hpp"
 
-#include "motor.hpp"
 #include <cmath>
-
-#define INIT_X M_PI / 2
-#define INIT_Y M_PI / 2
 
 #define a1 0.58
 #define a2 0.58
 
+#define INIT_X a1 + a2 - 0.01
+#define INIT_Y 0
+
 #define AXIS_LINEAR 1
 #define AXIS_ANGULAR 3
 
-#define SCALE_CONSTANT 1
+#define JOY_FREQUENCY 50.0
 
-#define JOY_FREQUENCY 50
+#define SCALE_CONSTANT 0.01 / JOY_FREQUENCY
 
 struct joint_angles_t
 {
@@ -50,19 +45,22 @@ public:
         joy_subscriber = this->create_subscription<sensor_msgs::msg::Joy>(
             "/joy", 10, std::bind(&ArmJoyControl::joy_callback, this, _1));
 
-        joint_angle_publisher = this->create_publisher<std_msgs::msg::Float64MultiArray>("/joint_positions_radians", 10);
+        joint_angle_publisher = this->create_publisher<std_msgs::msg::Float32MultiArray>("/joint_positions_radians", 10);
         cartesian_position.x = INIT_X;
         cartesian_position.y = INIT_Y;
+
+        RCLCPP_INFO(this->get_logger(), "Arm Joystick Control Started");
     }
 
 private:
     rclcpp::Subscription<sensor_msgs::msg::Joy>::SharedPtr joy_subscriber;
-    rclcpp::Publisher<std_msgs::msg::Float64MultiArray>::SharedPtr joint_angle_publisher;
+    rclcpp::Publisher<std_msgs::msg::Float32MultiArray>::SharedPtr joint_angle_publisher;
     cartesian_position_t cartesian_position;
 
     float compute_theta_2(float x, float y)
     {
-        return std::acos(pow(x, 2) + pow(y, 2) - pow(a1, 2) - pow(a2, 2)) / (2 * a1 * a2);
+        float arccos_number = (pow(x, 2) + pow(y, 2) - pow(a1, 2) - pow(a2, 2)) / (2 * a1 * a2);
+        return std::acos(arccos_number);
     }
 
     float compute_theta_1(float x, float y, float q2)
@@ -80,19 +78,23 @@ private:
 
     void joy_callback(sensor_msgs::msg::Joy::SharedPtr msg)
     {
-        float l_y = msg->axes[AXIS_LINEAR];
-        float l_x = msg->axes[AXIS_ANGULAR];
+        float l_y = (std::abs(msg->axes[1]) > 0.05) ? msg->axes[1] : 0;
+        float l_x = (std::abs(msg->axes[0]) > 0.05) ? msg->axes[0] : 0;
+        float base_duty = (std::abs(msg->axes[3]) > 0.05) ? msg->axes[3] : 0;
 
-        float dx = cartesian_position.x + (l_y * SCALE_CONSTANT);
-        float dy = cartesian_position.y + (l_x * SCALE_CONSTANT);
+        float dx = (l_x * SCALE_CONSTANT * 1.0);
+        float dy = (l_y * SCALE_CONSTANT * 1.0);
 
-        dx = (std::abs(dx) > 0.01/JOY_FREQUENCY) ? (0.01 * signum(dx)) : dx;
-        dy = (std::abs(dy) > 0.01/JOY_FREQUENCY) ? (0.01 * signum(dy)) : dy;
+        if (!(pow(cartesian_position.x + dx, 2) + pow(cartesian_position.y + dy, 2) > pow((a1 + a2), 2)))
+        {
+            cartesian_position.x += dx;
+            cartesian_position.y += dy;
+        }
 
-        joint_angles_t joint_angles = compute_angles(dx, dy);
+        joint_angles_t joint_angles = compute_angles(cartesian_position.x, cartesian_position.y);
 
-        auto message = std_msgs::msg::Float64MultiArray();
-        std::vector<double> positions = {0, joint_angles.q1, joint_angles.q2, 0};
+        auto message = std_msgs::msg::Float32MultiArray();
+        std::vector<float> positions = {base_duty, joint_angles.q1, joint_angles.q2, 0};
         message.data = positions;
         joint_angle_publisher->publish(message);
     }
