c217559ca4459cb5093ae81d58e99d410f2ebb60
LeScooot <jeronimus.ryan@gmail.com>
2026-02-20 15:56:05 -0500

    removing structs

--- anubis_ws/src/arm/src/arm_joy_control_node.cpp
+++ anubis_ws/src/arm/src/arm_joy_control_node.cpp
@@ -12,36 +12,7 @@
 
 #include "motor.hpp"
 
-#define MAX_MOTORS 4
 
-#define RADIAN_TO_REV 0.15915494
-
-#define BASE_LAT_GR 1
-#define BASE_JOINT_GR 125
-#define ELBOW_JOINT_GR 108
-#define END_EFFECTOR_GR 45
-float GEAR_RATIOS[4] = {BASE_LAT_GR, BASE_JOINT_GR, ELBOW_JOINT_GR, END_EFFECTOR_GR};
-
-enum JOINT
-{
-  BASE_LAT = 0,
-  BASE_JOINT,
-  ELBOW,
-  END_EFFECTOR
-};
-
-struct joint_motor_publishers
-{
-  rclcpp::Publisher<motor_messages::msg::Command>::SharedPtr left_publisher;
-  rclcpp::Publisher<motor_messages::msg::Command>::SharedPtr right_publisher;
-};
-
-struct joint_motors
-{
-
-  std::shared_ptr<motor> left;
-  std::shared_ptr<motor> right;
-};
 
 using std::placeholders::_1;
 class ArmJoyControl : public rclcpp::Node
